58eca13ca11aa9333ea009f947e8a5c068484b6a
Jaime <jaimelabana@gmail.com>
2025-06-17 16:57:55 +0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}", // this scans all Angular templates and components
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
